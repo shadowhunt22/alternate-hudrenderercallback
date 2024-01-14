de83259ac29ae645ffb0eb600c4e0e9b04cb8845
Kenner Hartman <155584978+shadowhunt22@users.noreply.github.com>
2024-01-13 20:41:57 -0700

    init commit

---
+++ src/client/java/com/shadowhunter22/AlternateHudRendererCallbackClient.java
@@ -0,0 +1,9 @@
+package com.shadowhunter22;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlternateHudRendererCallbackClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
